d3f515626434f65a551275702c8a23a2ef0d661f
rathanarajr <72396655+rathanarajr@users.noreply.github.com>
2022-09-08 18:35:50 +0530

    Update MyController.java

--- src/main/java/com/jboss/spring/MyController.java
+++ src/main/java/com/jboss/spring/MyController.java
@@ -11,7 +11,7 @@ public class MyController {
 	public String sayHello() {
 		
 //return "<html><body><h2> Hello LMA AR Stakeholder, Containerized version of Sample Application is running on JBOSS EAP 7.4.Please load other applications binary/archive to containerize other applications </h2></body></html>";
-		
+	return "index";
 	}
 
 	
